5517218ece945899eee640de51d23e1a413ed5fe
Alvaro-de-Amorim <alvaroamorimribeiro8@gmail.com>
2025-12-12 20:53:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnNumber = document.getElementsByClassName('btn-number');
+const btnFunc = document.getElementsByClassName('btn-func');
+const btnOpe = document.getElementsByClassName('btn-ope');
+const btnIgual = document.getElementsByClassName('igual');
+const primeiraLinha = document.getElementById('primeiraLinha');
+
+btnNumber.addEventListener('click', () => {
+      
+})
+btnFunc.addEventListener('click', () => {
+
+})
+btnOpe.addEventListener('click', () => {
+
+})
+btnIgual.addEventListener('click', () => {
+
+})
+primeiraLinha.addEventListener('click', () => {
+
+})
